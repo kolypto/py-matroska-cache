62cb008fd11851c5822f1ad2d48e852f13cfdaee
Mark Vartanyan <kolypto@gmail.com>
2020-09-08 23:23:05 +0300

    dependencies: NTag: a namespaced tag

---
+++ matroska_cache/dependency/ntag.py
@@ -0,0 +1,16 @@
+from .tag import Tag, dataclass
+
+
+@dataclass
+class NTag(Tag):
+    """ Namespaced tag that uses its class name as the prefix.
+
+    Use it in a large application to have more control over prefixes
+    """
+    name: str
+    __slots__ = 'name',
+
+    PREFIX = ...
+
+    def __init_subclass__(cls):
+        cls.PREFIX = cls.__name__
